7473a5b98fa705c44afb30ef8bb6ff65752eb400
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-08-06 23:06:55 +0100

    Update README and enhance Logo component styling

--- app/_components/Home.js
+++ app/_components/Home.js
@@ -3,10 +3,10 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
-      <HomeIcon className="w-6 h-6" />
+    <Link href="/" className="flex gap-4 z-10">
+      <HomeIcon className="w-8 h-8" />
 
-      <span className="text-base sm:text-xl font-semibold text-primary-100">
+      <span className="text-lg sm:text-3xl font-semibold text-primary-100">
         Sanni Abdulmuiz A.
       </span>
     </Link>
