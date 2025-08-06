84adde30845271020a20db3b9cf2cb49ddbd5d59
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-08-06 23:08:02 +0100

    Add Button component for downloading resume

---
+++ app/_components/Button.js
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <button className="bg-pink-500 hover:bg-fuchsia-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:shadow-fuchsia-500/50 transition duration-300">
+      Download Resume
+    </button>
+  );
+}
+
+export default Button;
