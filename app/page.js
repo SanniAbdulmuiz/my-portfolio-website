d2f7f32c4cbe63369a4915fb1f661584f9e5ad52
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-08-07 21:55:30 +0100

    Refactor Page component for improved readability and maintainability

--- app/page.js
+++ app/page.js
@@ -1,3 +1,24 @@
 export default function Page() {
-  return <h1>Hello next</h1>;
+  return (
+    <main className="mt-12">
+      <div className="flex items-center justify-center">
+        <div className="max-w-2xl text-center px-4">
+          <h3 className="text-2xl text-blue-100 mb-10 tracking-tight font-normal">
+            Hello! I&apos;m,
+          </h3>
+          <h1 className="text-6xl text-fuchsia-500 mb-10 tracking-tight font-bold">
+            Abdulmuiz
+          </h1>
+          <h2 className="text-4xl text-blue-50 mb-10 tracking-tight font-semibold">
+            A Frontend Web Developer.
+          </h2>
+          <p className="text-2xl text-blue-100 mb-10 tracking-tight font-medium">
+            I craft engaging, high-performance web experiences using modern
+            technologies — where clean code meets elegant design and purposeful
+            innovation.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
