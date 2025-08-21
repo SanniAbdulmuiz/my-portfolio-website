1df1f2d52201a0393a14a09df4847a3d275c555b
Sanni Abdulmuiz <ayomideabdulmuiz565@gmail.com>
2025-08-21 18:27:40 +0100

    Refactor MyProjects component: update tech stack for Wurana project for accuracy

--- app/_components/MyProjects.js
+++ app/_components/MyProjects.js
@@ -43,7 +43,7 @@ const projects = [
     description:
       "Volunteered as a frontend contributor for Wurana, a decentralized talent marketplace that connects users with skilled craftspeople through secure smart contracts, escrow payments, and NFT-based reputation badges.",
     image: project3,
-    tech: ["Typescript", "React", "React-Router", "Tailwind CSS"],
+    tech: ["Typescript", "React", "React-Router", "Vite", "Tailwind CSS"],
     liveLink: "#",
   },
 ];
